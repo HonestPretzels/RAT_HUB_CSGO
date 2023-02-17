52e974d4e535afc2b2e2d1fac560f3b61de4b32b
Tom <tom.maurer@vizworx.com>
2023-02-17 15:40:40 -0700

    replace secret key

--- backend/rat_hub/rat_hub/settings.py
+++ backend/rat_hub/rat_hub/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^wm(ub%td9#6(5@=(*a3s2j_j#0(vtkgiw-n+erbe$lsbfq%se'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
