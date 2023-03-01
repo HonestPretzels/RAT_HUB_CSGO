9fd9b07f099b27956f735940fc63db83fda86857
Tom <tom.maurer@vizworx.com>
2023-03-01 12:32:45 -0700

    Chakra install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,10 @@
+import {
+	Box,
+	ChakraProvider,
+	ColorModeScript,
+	useColorModeValue,
+} from "@chakra-ui/react";
+
 import "./index.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -8,27 +15,31 @@ import Home from "./views/homePage";
 import Login from "./views/loginPage";
 import Register from "./views/registerPage";
 import ProtectedPage from "./views/ProtectedPage";
+import theme from "./theme/theme";
 
 function App() {
 	return (
-		<Router>
-			<div className="flex flex-col min-h-screen overflow-hidden">
-				<AuthProvider>
-					<Navbar />
-					<Switch>
-						<PrivateRoute
-							component={ProtectedPage}
-							path="/protected"
-							exact
-						/>
-						<Route component={Login} path="/login" />
-						<Route component={Register} path="/register" />
-						<Route component={Home} path="/" />
-					</Switch>
-				</AuthProvider>
-				<Footer />
-			</div>
-		</Router>
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<Router>
+				<Box bg={useColorModeValue("gray.100", "gray.900")}>
+					<AuthProvider>
+						<Navbar />
+						<Switch>
+							<PrivateRoute
+								component={ProtectedPage}
+								path="/protected"
+								exact
+							/>
+							<Route component={Login} path="/login" />
+							<Route component={Register} path="/register" />
+							<Route component={Home} path="/" />
+						</Switch>
+					</AuthProvider>
+					<Footer />
+				</Box>
+			</Router>
+		</ChakraProvider>
 	);
 }
 
